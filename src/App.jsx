964cd4bedf4d3067323feb5382ef1382dd059138
Dima Shalupnya <dshalupnya@gmail.com>
2026-03-17 15:32:14 +0100

    wip

--- src/App.jsx
+++ src/App.jsx
@@ -20,6 +20,19 @@ function Header() {
 }
 
 
+function Headers() {
+  const description = reactDescriptions[genRandomInt(2)];
+  return (<header>
+    <img src={reactImg} alt="Stylized atom" />
+    <h1>React Essentials</h1>
+    <p>
+      {description} React concepts you will need for almost any app you are
+      going to build!
+    </p>
+  </header>
+  );
+}
+
 
 function App() {
   return (
