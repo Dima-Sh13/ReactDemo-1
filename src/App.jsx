e204f4588bc909a980c476e182438101e4328d04
Dima Shalupnya <dshalupnya@gmail.com>
2026-03-22 20:38:18 +0100

    structure and best practices

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,9 @@ import Header from './components/Header.jsx';
 
 
 
+
+
+
 function App() {
   return (
     <div>
@@ -14,11 +17,13 @@ function App() {
         <section id="core-concepts">
         <h2>Core Concepts</h2>
         <ul>
+          
           <CoreConcept title="Components" description="The core UI building block, that adds complexity and functionality to our code" 
-          img={componentsImg}/>
+          img={CORE_CONCEPTS[0].img}/>
           <CoreConcept title={CORE_CONCEPTS[2].title} img={CORE_CONCEPTS[2].img} description="Data we pass to react components that make them show dynamic data with only 1 component. We do this with HTML"></CoreConcept>
           <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} img={CORE_CONCEPTS[1].img}></CoreConcept>
           <CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
+          
         </ul>
 
         </section>
