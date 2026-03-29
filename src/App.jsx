240f4b4b3a89c37a765a256345dc888c90636c5c
Dima Shalupnya <dshalupnya@gmail.com>
2026-03-29 21:26:37 +0200

    dynamic cs 2

--- src/App.jsx
+++ src/App.jsx
@@ -40,10 +40,10 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect(CORE_CONCEPTS[0].title)}>{CORE_CONCEPTS[0].title}</TabButton>
-            <TabButton onSelect={() => handleSelect(CORE_CONCEPTS[1].title)}>{CORE_CONCEPTS[1].title}</TabButton>
-            <TabButton onSelect={() => handleSelect(CORE_CONCEPTS[2].title)}>{CORE_CONCEPTS[2].title}</TabButton>
-            <TabButton onSelect={() => handleSelect(CORE_CONCEPTS[3].title)}>{CORE_CONCEPTS[3].title}</TabButton>
+            <TabButton isSelected={selectedTopic === CORE_CONCEPTS[0].title } onSelect={() => handleSelect(CORE_CONCEPTS[0].title)}>{CORE_CONCEPTS[0].title}</TabButton>
+            <TabButton isSelected={selectedTopic === CORE_CONCEPTS[1].title } onSelect={() => handleSelect(CORE_CONCEPTS[1].title)}>{CORE_CONCEPTS[1].title}</TabButton>
+            <TabButton isSelected={selectedTopic === CORE_CONCEPTS[2].title } onSelect={() => handleSelect(CORE_CONCEPTS[2].title)}>{CORE_CONCEPTS[2].title}</TabButton>
+            <TabButton isSelected={selectedTopic === CORE_CONCEPTS[3].title } onSelect={() => handleSelect(CORE_CONCEPTS[3].title)}>{CORE_CONCEPTS[3].title}</TabButton>
             
           </menu>
             {!selectedTopic ? <p>Please select a topic.</p>: <div id='tab-content'>
