37130dac783c2c46a4f52da7df266235b08341cf
Dima Shalupnya <dshalupnya@gmail.com>
2026-03-30 23:43:11 +0200

    dynamic list rendering

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,9 @@ function App() {
         <section id="core-concepts">
         <h2>Core Concepts</h2>
         <ul>
+          {CORE_CONCEPTS.map((item) => <CoreConcept {...item } />)}
           
-          <CoreConcept title="Components" description="The core UI building block, that adds complexity and functionality to our code" 
-          img={CORE_CONCEPTS[0].img}/>
-          <CoreConcept title={CORE_CONCEPTS[2].title} img={CORE_CONCEPTS[2].img} description="Data we pass to react components that make them show dynamic data with only 1 component. We do this with HTML"></CoreConcept>
-          <CoreConcept title={CORE_CONCEPTS[1].title} description={CORE_CONCEPTS[1].description} img={CORE_CONCEPTS[1].img}></CoreConcept>
-          <CoreConcept {...CORE_CONCEPTS[3]}></CoreConcept>
-          
+
         </ul>
 
         </section>
